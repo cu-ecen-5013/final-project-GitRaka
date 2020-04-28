516c9b62856c485db7f3bc98d9b1a2cb0c76a90c
GitRaka <raku9037@colorado.edu>
2020-04-28 02:17:43 -0600

    Commit for multiple topic subscription implementation to handle multiple publishes.

---
+++ Subscribe_Image_Test/test2.py
@@ -0,0 +1,14 @@
+# Reference: https://stackoverflow.com/questions/37499739/how-can-i-send-a-image-by-using-mosquitto
+# Reference http://www.steves-internet-guide.com/into-mqtt-python-client/
+# Rakesh Kumar
+
+import paho.mqtt.publish as publish
+MQTT_SERVER = "localhost"
+MQTT_PATH = "Info"
+
+f=open("image.jpg", "rb")
+fileContent = f.read()
+byteArr = bytearray(fileContent)
+
+
+publish.single(MQTT_PATH, byteArr, hostname=MQTT_SERVER)
